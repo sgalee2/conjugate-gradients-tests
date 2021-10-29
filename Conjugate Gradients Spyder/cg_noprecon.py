c519a2aef18e6d87fcae3910debd17b7ff002a0d
AJL-1998 <73225992+AJL-1998@users.noreply.github.com>
2021-10-29 13:00:47 +0100

    Changes to test

--- Conjugate Gradients Spyder/cg_noprecon.py
+++ Conjugate Gradients Spyder/cg_noprecon.py
@@ -47,13 +47,43 @@ def protein_cg(lengthscale_range, noise_range, kernel, ARD=False, n=1000):
                 #run cg
                 cg_run = cg_test(lin_cg, K_hat, train_y, sol=sol)
                 #store results
-                results = [[ave, sigma, cg_run[0], cg_run[1]]]
+                results = [[int(np.log(ave)), int(np.log(sigma)), cg_run[0], cg_run[1]]]
+                res = np.vstack([res, results])
+        #return results as a dataframe
+        res = pandas.DataFrame( res[1:, 0:3], columns=['Lengthscales', 'Noise', 'Iterations'])
+        return res
+    else:
+        covar_fn = kernel()
+        for i in range(num_trials):
+            
+            #compute lengthscales for current batch
+            
+            #initialise lengthscales in cov
+            covar_fn.lengthscale = lengthscale_range[i]
+            x_tens = torch.Tensor(train_x)
+            #compute cov
+            K_XX = covar_fn(x_tens, x_tens).evaluate().detach().numpy()
+            for j in range(noise_length):
+                #assign noise & compute likelihood cov
+                sigma = noise_range[j]
+                K_hat = K_XX + sigma ** 2 * np.eye(n)
+                #compute exact solution for error comparison
+                sol = np.linalg.inv(K_hat) @ train_y
+                #run cg
+                cg_run = cg_test(lin_cg, K_hat, train_y, sol=sol)
+                #store results
+                results = [[int(np.log(lengthscale_range[i])), int(np.log(sigma)), cg_run[0], cg_run[1]]]
                 res = np.vstack([res, results])
         #return results as a dataframe
         res = pandas.DataFrame( res[1:, 0:3], columns=['Lengthscales', 'Noise', 'Iterations'])
         return res
 
-cg_ = protein_cg(np.linspace(0.0,1.0,10), [1,2,3,4,5,6,7,8], RBFKernel, ARD=1)
+ls_ = [10 ** i for i in range(-3,3)]
+sig_ = [10 ** i for i in range (-3, 3)]
+cg_ = protein_cg(ls_, sig_, RBFKernel, ARD=0)
+
 heat_map = cg_.pivot('Lengthscales', 'Noise', 'Iterations')
 plt.figure(figsize=[10,10])
-seaborn.heatmap(heat_map, cmap='Reds')
+plt.title('CG Iterations (log scale)')
+seaborn.heatmap(np.log(heat_map), cmap='Reds')
+plt.show()
