799935322c7c791118a7f3df3292342c48bf20fd
AJL-1998 <73225992+AJL-1998@users.noreply.github.com>
2021-11-05 08:57:16 +0000

    Removed optimal analysis
    Some issues with complex arithmetic when computing M^{-1}

--- Conjugate Gradients Spyder/cg_optimalprecon.py
+++ Conjugate Gradients Spyder/cg_optimalprecon.py
@@ -11,39 +11,4 @@ from linear_conjugate_gradients import *
 import sklearn.datasets as dt
 
 
-ls_ = [10 ** i for i in range(-4,4)]
-sig_ = [10 ** i for i in range(-2, 4)]
-n = 10
-rank = int(np.sqrt(n))
-vmax = int(np.log10(n)) + 1
 
-pcg_p1 = cg_(load_protein, ls_, sig_, form_cov, ARD = False, n=n, p_strat = optimal_precon, rank = rank)
-cg_p1  = cg_(load_protein, ls_, sig_, form_cov, ARD = False, n=n)
-pcg_p2 = cg_(load_protein, ls_, sig_, form_cov, ARD = True, n=n, p_strat = optimal_precon, rank = rank)
-cg_p2  = cg_(load_protein, ls_, sig_, form_cov, ARD = True, n=n)
-
-print(cg_p1.values[:,2] - pcg_p1.values[:,2])
-
-
-pcg_e1 = cg_(load_elevator, ls_, sig_, form_cov, ARD = False, n=n, p_strat = optimal_precon, rank = rank)
-cg_e1  = cg_(load_elevator, ls_, sig_, form_cov, ARD = False, n=n)
-pcg_e2 = cg_(load_elevator, ls_, sig_, form_cov, ARD = True, n=n, p_strat = optimal_precon, rank = rank)
-cg_e2  = cg_(load_elevator, ls_, sig_, form_cov, ARD = True, n=n)
-
-
-pcg_b1 = cg_(load_bike, ls_, sig_, form_cov, ARD = False, n=n, p_strat = optimal_precon, rank = rank)
-cg_b1  = cg_(load_bike, ls_, sig_, form_cov, ARD = False, n=n)
-pcg_b2 = cg_(load_bike, ls_, sig_, form_cov, ARD = True, n=n, p_strat = optimal_precon, rank = rank)
-cg_b2  = cg_(load_bike, ls_, sig_, form_cov, ARD = True, n=n)
-
-
-pcg_r1 = cg_(load_road, ls_, sig_, form_cov, ARD = False, n=n, p_strat = optimal_precon, rank = rank)
-cg_r1  = cg_(load_road, ls_, sig_, form_cov, ARD = False, n=n)
-pcg_r2 = cg_(load_road, ls_, sig_, form_cov, ARD = True, n=n, p_strat = optimal_precon, rank = rank)
-cg_r2  = cg_(load_road, ls_, sig_, form_cov, ARD = True, n=n)
-
-
-pcg_t1 = cg_(load_2dtoy, ls_, sig_, form_cov, ARD = False, n=n, p_strat = optimal_precon, rank = rank)
-cg_t1  = cg_(load_2dtoy, ls_, sig_, form_cov, ARD = False, n=n)
-pcg_t2 = cg_(load_2dtoy, ls_, sig_, form_cov, ARD = True, n=n, p_strat = optimal_precon, rank = rank)
-cg_t2  = cg_(load_2dtoy, ls_, sig_, form_cov, ARD = True, n=n)
